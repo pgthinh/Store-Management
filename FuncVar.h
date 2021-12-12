6fedc67e7eeaf3df9e74d5a143f904baff35a6d5
Gia-Thinh Pham <87513296+pgthinh@users.noreply.github.com>
2021-12-12 21:42:31 +0700

    Update FuncVar.h

--- FuncVar.h
+++ FuncVar.h
@@ -1,9 +1,9 @@
 ///===== Variables =====
 //Menu
-string main_menu[8] = {" 1. Add & Current Item ", " 2. Restock ", " 3. Revenue & Profit ", " 4. Find & Delete ", " 5. Update Price & Quantity ", " 6. Customer Service "," 7. Employee ", " 8. Exit "};
-const int main_menu_len = 8;
-string customer_menu[4] = {" 1. Get Bill ", " 2. Customer Infor ", " 3. Rate & Feedback ", " 4. Return "};
-const int customer_menu_len = 4;
+string main_menu[9] = {" 1. Add & Current Item ", " 2. Restock ", " 3. Revenue & Profit ", " 4. Find & Delete ", " 5. Update Price & Quantity ", " 6. Customer Service "," 7. Employee ", " 8. Change Password"," 9. Exit "};
+const int main_menu_len = 9;
+string customer_menu[5] = {" 1. Get Bill ", " 2. Customer Infor ", " 3. Rate & Feedback ", " 4. Find Items", " 5. Return "};
+const int customer_menu_len = 5;
 string yes_no_menu[2] = {" YES ", " NO "};
 const int yes_no_menu_len = 2;
 string yes_no_menu1[3] = {" Price ", " Amount ", " Both "};
@@ -16,6 +16,7 @@ string yes_no_menu4[2] = {" 1. Rate for us ", " 2. See feedback "};
 const int yes_no_menu_len4 = 2;
 string employee_menu[5] = {" 1. List ", " 2. Check-in ", " 3. Check-out ", " 4. Statistics ", " 5. Return "};
 const int employee_menu_len = 5;
+string defaultpass = "123";
 
 ///===== Function Prototype
 struct billinfor{
@@ -43,6 +44,8 @@ int WhichColor(char Cell);
 void Welcome();
 int menu(string Menu[], int menu_len, string type, int x, int y, bool vertical);
 void show_menu(string Menu[], int menu_len, int choose, string type, int x, int y, bool vertical);
+bool password();
+void changepassword();
 
 //music.h
 void Music();
@@ -57,6 +60,7 @@ void updatebill();
 void takebill();
 void updateinfor();
 void takeinfor();
+
 //Restock.h
 void restock();
 
